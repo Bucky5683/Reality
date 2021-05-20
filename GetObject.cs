b24f65fb62d7f1d7c6b74113eb4b8c958e204585
wintery <67188355+winter-y@users.noreply.github.com>
2021-05-21 00:53:10 +0900

    Add files via upload

---
+++ GetObject.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetObject : MonoBehaviour
+{
+    public Camera getCamera; //카메라받아옴
+    private Object target; //현재오브젝트
+    private RaycastHit hit;
+    private string object_name; //오브젝트 이름
+
+    void Start()
+    {
+        target = GameObject.Find(this.name).GetComponent<Object>(); //현재의 오브젝트를 가져옴
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //왼쪽클릭시
+        {
+            if (checking() == this.name)//마우스포인터가 현재 오브젝트에 있을 경우
+            {
+                if (target.rightclick == false && target.leftclick == false)
+                {
+                    //타겟의 오른쪽 클릭 X, 왼쪽 클릭 X일 경우 왼쪽 클릭을 true로
+                    target.rightclick = false;
+                    target.leftclick = true;
+                }
+                else if (target.rightclick == true && target.leftclick == false)
+                {
+                    //타겟의 오른쪽 클릭 O, 왼쪽 클릭 X일 경우 왼쪽 클릭을 true로, 오른쪽클릭을 해제
+                    target.rightclick = false;
+                    target.leftclick = true;
+                }
+                else
+                {
+                    //타겟의 왼쪽클릭이 O일경우, 오른쪽 왼쪽 클릭 해제
+                    target.leftclick = false;
+                    target.rightclick = false;
+                }
+            }
+            else//마우스포인터가 현재 오브젝트에 없을 경우
+            {
+                //타겟의 왼쪽 오른쪽 클릭 해제
+                target.leftclick = false;
+                target.rightclick = false;
+
+            }
+        }
+        else if (Input.GetMouseButtonDown(1)) //오른쪽 클릭한 경우 -> 왼쪽클릭했을 때처럼 진행
+        {
+            //Debug.Log("Pressed right click");
+            if (checking() == this.name)
+            {
+                if (target.leftclick == false && target.rightclick == false)
+                {
+                    target.leftclick = false;
+                    target.rightclick = true;
+                }
+                else if(target.leftclick == true && target.rightclick == false)
+                {
+                    target.leftclick = false;
+                    target.rightclick = true;
+                }
+                else
+                {
+                    target.rightclick = false;
+                    target.leftclick = false;
+                }
+            }
+            else
+            {
+                target.rightclick = false;
+                target.leftclick = false;
+            }
+        }
+    }
+
+    string checking() //마우스포인터가 오브젝트에 있는지 없는지 확인 -> 정상작동 되므로 신경 X
+    {
+        Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            object_name = hit.collider.gameObject.name;
+        }
+        //Debug.Log(object_name);
+        return object_name;
+    }
+}
---
+++ Maincamera.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maincamera : MonoBehaviour
+{
+    private Vector3 firstlocation = new Vector3(0, 13, -20); //메인카메라 처음위치
+    private Cylindertest target_object; // 선택된 오브젝트 받아오기 위해서
+    public Object target;        // 따라다닐 타겟 오브젝트의 Transform
+    private float speed_rota = 2.0f;
+    public Transform tr;         // 카메라 자신의 Transform
+    
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        target_object = GameObject.Find("Canvas").GetComponent<Cylindertest>();
+    }
+
+    void LateUpdate()
+    {
+        
+        if (target == null || (target.rightclick == false && target.leftclick == false))
+        {
+            /*if (target != null)
+                Debug.Log("Maincamera: No checked!");
+            else
+                Debug.Log("Maincamera: target is null!");*/
+            tr.position = firstlocation;
+            tr.rotation = Quaternion.Euler(30, 0, 0);
+        }
+        else if (target.rightclick == true || target.leftclick == true)
+        {
+            if (target.leftclick == true)
+            {
+                //Debug.Log("Maincamera: Left checked!");
+                tr.position = new Vector3(target.object_name.position.x - 0.52f, tr.position.y, target.object_name.position.z - 6.56f);
+                tr.LookAt(target.object_name);
+            }
+            else if (target.rightclick == true)
+            {
+                //Debug.Log("Maincamera: right checked!");
+                tr.position = new Vector3(target.object_name.position.x, target.object_name.position.y, target.object_name.position.z);
+                float mouseX = Input.GetAxis("Mouse X");
+                float mouseY = Input.GetAxis("Mouse Y");
+                transform.Rotate(Vector3.up * speed_rota * mouseX);
+                transform.Rotate(Vector3.left * speed_rota * mouseY);
+            }
+            else
+            {
+                //Debug.Log("Maincamera: both checked!");
+                tr.position = firstlocation;
+                tr.rotation = Quaternion.Euler(30, 0, 0);
+                /*tr.position = new Vector3(target.object_name.position.x - 0.52f, tr.position.y, target.object_name.position.z - 6.56f);
+                tr.LookAt(target.object_name);*/
+            }
+        }
+        if(target != null)
+        {
+            Debug.Log("Maincamera: " + target.object_name.name + " leftclick = " + target.leftclick + " rightclick = " + target.rightclick);
+        }
+    }
+}
