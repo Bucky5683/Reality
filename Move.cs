bbb47e781c81f0dfc53e6e5fa6709e6fae04b160
wintery <67188355+winter-y@users.noreply.github.com>
2021-06-09 01:39:07 +0900

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int objectID = -1;
+    private void Start()
+    {
+        //Debug.Log("옹");
+        
+    }
+    private void OnEnable()     // 오브젝트가 활성화되면 실행하는 함수
+    {
+
+    }
+    private void Update()
+    {
+
+    }
+}
