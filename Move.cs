0a15718b19f5e7468d88ef57966c322278c04707
wintery <67188355+winter-y@users.noreply.github.com>
2021-06-13 23:17:20 +0900

    찐찐찐막2

--- Move.cs
+++ Move.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public int objectID = -1;
+    public int objectID = 0;
     private void Start()
     {
         //Debug.Log("옹");
-        
+        Debug.Log(objectID);
     }
     private void OnEnable()     // 오브젝트가 활성화되면 실행하는 함수
     {
